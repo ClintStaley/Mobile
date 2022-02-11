a761132ca97d6b8562515a560c3f7a7573d770aa
Clint Staley <clintstaley@gmail.com>
2022-02-11 14:18:03 -0600

    homeworks

--- AndroidProjects/TipTime/app/src/main/java/com/example/tiptime/MainActivity.kt
+++ AndroidProjects/TipTime/app/src/main/java/com/example/tiptime/MainActivity.kt
@@ -2,11 +2,12 @@ package com.example.tiptime
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.example.tiptime.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var binding: ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -15,7 +16,23 @@ class MainActivity : AppCompatActivity() {
         binding.calculateTip.setOnClickListener{calculateTip()}
     }
 
-    fun calculateTip() {
+    private fun calculateTip() {
+        val cost = binding.serviceCost.text.toString().toDoubleOrNull()
+        val tipPct = when (binding.tipOptions.checkedRadioButtonId) {
+            R.id.option_20 -> 0.20
+            R.id.option_18 -> 0.18
+            else -> 0.15
+        }
+        val roundUp = binding.tipRound.isChecked
 
+        if (cost != null) {
+            Log.e("com.example.tiptime","Cost is $cost")
+            val tip = if (roundUp) kotlin.math.ceil(cost * tipPct) else cost * tipPct
+
+            binding.tipResult.text = "Tip: $tip"
+            binding.tipRound.setChecked(false)
+        }
+        else
+            binding.tipResult.text = ""
     }
 }
